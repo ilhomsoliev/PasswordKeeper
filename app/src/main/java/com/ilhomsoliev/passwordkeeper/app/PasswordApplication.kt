a67f6913a087f80904bedc6c57641b7fb8eb082f
Ilhom Soliev <soliev.ilhom.2005@gmail.com>
2024-03-17 16:31:36 +0300

    proper di added

--- app/src/main/java/com/ilhomsoliev/passwordkeeper/app/PasswordApplication.kt
+++ app/src/main/java/com/ilhomsoliev/passwordkeeper/app/PasswordApplication.kt
@@ -5,28 +5,16 @@ import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.disk.DiskCache
 import coil.memory.MemoryCache
-import com.ilhomsoliev.passwordkeeper.core.CryptoManager
-import com.ilhomsoliev.passwordkeeper.data.local.dao.WebsitePasswordDao
-import com.ilhomsoliev.passwordkeeper.data.local.db.ApplicationDatabase
-import com.ilhomsoliev.passwordkeeper.data.local.db.getDatabaseInstance
-import com.ilhomsoliev.passwordkeeper.data.local.valueBased.DataStoreManager
-import com.ilhomsoliev.passwordkeeper.data.repository.WebsitePasswordRepositoryImpl
-import com.ilhomsoliev.passwordkeeper.domain.repository.WebsitePasswordRepository
-import com.ilhomsoliev.passwordkeeper.domain.usecase.CheckIsFirstTimeUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.CheckMasterPasswordUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.DeleteWebsitePasswordUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.GetWebsitePasswordByIdUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.GetWebsitesPasswordsUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.InsertWebsitePasswordUseCase
-import com.ilhomsoliev.passwordkeeper.domain.usecase.StoreMasterPasswordUseCase
-import com.ilhomsoliev.passwordkeeper.feature.add.vm.AddPasswordViewModel
-import com.ilhomsoliev.passwordkeeper.feature.home.vm.HomeViewModel
+import com.ilhomsoliev.passwordkeeper.app.di.dataStore
+import com.ilhomsoliev.passwordkeeper.app.di.database
+import com.ilhomsoliev.passwordkeeper.app.di.manager
+import com.ilhomsoliev.passwordkeeper.app.di.repository
+import com.ilhomsoliev.passwordkeeper.app.di.usecases
+import com.ilhomsoliev.passwordkeeper.app.di.viewmodel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.koin.dsl.module
 
 class PasswordApplication : Application(), ImageLoaderFactory {
     override fun onCreate() {
@@ -35,31 +23,17 @@ class PasswordApplication : Application(), ImageLoaderFactory {
     }
 
     private fun initDI() {
-        val viewModelModule = module {
-            viewModel { HomeViewModel(get(), get(), get(), get(), get()) }
-            viewModel { AddPasswordViewModel(get(), get(), get()) }
-        }
         startKoin {
             androidLogger(Level.DEBUG)
             androidContext(this@PasswordApplication)
             modules(
                 listOf(
-                    module {
-                        single<ApplicationDatabase> { getDatabaseInstance(androidContext()) }
-                        single<WebsitePasswordDao> { get<ApplicationDatabase>().websitePasswordDao() }
-                        single<WebsitePasswordRepository> { WebsitePasswordRepositoryImpl(get()) }
-                        single { DataStoreManager(androidContext()) }
-                        single { CryptoManager() }
-                        single { InsertWebsitePasswordUseCase(get(), get()) }
-                        single { GetWebsitesPasswordsUseCase(get()) }
-                        single { GetWebsitePasswordByIdUseCase(get(), get()) }
-                        single { DeleteWebsitePasswordUseCase(get()) }
-                        single { CheckIsFirstTimeUseCase(get()) }
-                        single { StoreMasterPasswordUseCase(get(), get()) }
-                        single { CheckMasterPasswordUseCase(get(), get()) }
-
-                    },
-                    viewModelModule
+                    dataStore(),
+                    database(),
+                    viewmodel(),
+                    repository(),
+                    manager(),
+                    usecases(),
                 )
             )
         }
@@ -85,3 +59,4 @@ class PasswordApplication : Application(), ImageLoaderFactory {
         }
         .build()
 }
+
--- app/src/main/java/com/ilhomsoliev/passwordkeeper/core/BiometricAuthStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.ilhomsoliev.passwordkeeper.core
-
-
-enum class BiometricAuthStatus(val code: Int) {
-    READY(1),
-    NOT_AVAILABLE(-1),
-    TEMPORARY_NOT_AVAILABLE(-2),
-    AVAILABLE_BUT_NOT_ENROLLED(-3),
-}
